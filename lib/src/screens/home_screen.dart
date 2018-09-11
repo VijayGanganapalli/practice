d5753c848ab56b6477c398e0e1238e732ecde617
Vijay <vijayganganapalli@gmail.com>
2018-09-11 06:38:03 +0530

    title added to appbar

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -9,7 +9,9 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Practice"),
+      ),
     );
   }
 }
