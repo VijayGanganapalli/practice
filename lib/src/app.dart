64efad27d674cb876c2b6153ee8167b9cdf850e9
Vijay <vijayganganapalli@gmail.com>
2018-09-11 06:25:11 +0530

    initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Practice",
+      home: HomeScreen(),
+    );
+  }
+}
